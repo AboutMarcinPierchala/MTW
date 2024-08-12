31be2e8af00399aa7f04b1c738fa75c3b75566be
MarcinPierchala <macintecmarcin@gmail.com>
2024-08-12 20:43:47 +0200

    add save data in AddInventory Page

--- MTW.CoreBusiness/Inventory.cs
+++ MTW.CoreBusiness/Inventory.cs
@@ -1,10 +1,19 @@
-﻿namespace MTW.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTW.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(1,int.MaxValue, ErrorMessage="Quantity must be greather than zero.")]
         public int Quantity { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be greather than zero.")]
         public double Price { get; set; }
     }
 }
